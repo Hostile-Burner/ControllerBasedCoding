7aacef3c77080eee46f5ffdbd75379fdbb326d5a
ADR203 <146063331+ADR203@users.noreply.github.com>
2026-05-03 18:16:11 -0700

    Update CBCLanguage.cpp
    Updated translate function

--- BackEnd/Language/CBCLanguage.cpp
+++ BackEnd/Language/CBCLanguage.cpp
@@ -6,6 +6,10 @@
 class CBC {
     private:
     std::unordered_map<int, std::string> env;
+    std::vector<std::string> nums = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
+    std::vector<std::string> eq = {"=", "+", "-", "*", "/", "()"};
+    std::vector<std::string> cond = {"if ()", "else", "else if ()"};
+    std::vector<std::string> special = {" ", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "{", "}", "[", "]", ";", ":", ",", ".", "?"};
 
     //used to trim extra spaces
     std::string trim(const std::string& str) {
@@ -16,36 +20,49 @@ class CBC {
         return std::string(start, end + 1);
     }
 
+    std::string pickFromCircle(int degrees, const std::vector<std::string>& options) {
+        if (options.empty()) return "";
+        // Divide 360 degrees
+        float segment = 360.0f / options.size();
+        int index = std::floor(degrees / segment);
+
+        //Clamp Index to prevent out of bounds at 360
+        if (index >= options.size()) index = options.size() - 1;
+        return options[index];
+    }
     //translate raw inputs into readable language
     std::string translate(std::string input){
         if (input.empty()) return "";
-        
-        //Identifies the category from the first character
-        char category = input[0];
-        //Extract the jostick degrees
-        int degrees = std::stoi(input.substr(1));
 
-        if (category == 'c') { 
-            if (degrees < 90) return "if (";
-            if (degrees >= 90 && degrees <180) return "loop ";
+        std::string result = "";
+        // Process chunks likes c024
+        for (size_t i = 0; i < input.length(); i+= 4) {
+            char identity = input [i];
+            int degrees = std::stoi(input.substr(i + 1, 3);
+
+            switch (identity) {
+                case 'n': //Numbers
+                    result += pickFromCircle(degrees, nums);
+                    break;
+                case 'e': //Equations
+                    result += pickFromCircle(degrees, eq);
+                    break;
+                case 'c': //Conditionals
+                    result += pickFromCircle(degrees, cond);
+                    break;
+                case 's': // Special characters
+                    result += pickFromCircle(degrees, special);
+                    break;
+                case 'a': //Variables (abc)
+                    result += "a"; //Placehold for alphabet logic
+                    break;
+            }
         }
-        else if (category == 'n') {
-            //Converts degree range to a specific number of variable identity
-            return "2";
-        }
-        else if (category == 'e') {
-            return ") {";
-        }
-        else if (category == 's') {
-            return "}" ;
-        }
-        return input; 
+        return result;
     }
     void run(std::string input){
-        // run calls 'translate' to convert "c024" into "if ("
-        std::string translatedCode = translate(input);
-
-        std::cout<< "Executing: " << translatedCode << std::endl;
+        std::string code = translate(input);
+        std::cout<< "Translating Code: " << code << std::endl;
     }
 
     public:
