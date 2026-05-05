d59194045af78a87e48ca9d8ac668a09edd75a3c
Hostile_Burner <161495934+Hostile-Burner@users.noreply.github.com>
2026-05-04 21:08:06 -0700

    Revert "Merge branch 'main' into HostileBranch"
    This reverts commit 3c986847d9f2223a77ebdebba19cee66ad51bcc1, reversing
    changes made to 1dba30df772c4436c9b62382197b233c9ba22c58.

--- BackEnd/Language/CBCLanguage.cpp
+++ BackEnd/Language/CBCLanguage.cpp
@@ -6,65 +6,45 @@
 
 class CBC {
     private:
-    std::unordered_map<int, std::string> env;
-    std::vector<std::string> nums = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
-    std::vector<std::string> eq = {"=", "+", "-", "*", "/", "()"};
-    std::vector<std::string> cond = {"if ()", "else", "else if ()"};
-    std::vector<std::string> special = {" ", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "{", "}", "[", "]", ";", ":", ",", ".", "?"};
+        std::unordered_map<int, std::string> env;
+        Category cat;
 
-    //used to trim extra spaces
-    std::string trim(const std::string& str) {
-        auto start = str.begin();
-        while (start != str.end() && std::isspace(*start)) ++start;
-        auto end = str.end();
-        do { --end; } while (end != start && std::isspace(*end));
-        return std::string(start, end + 1);
-    }
-
-    std::string pickFromCircle(int degrees, const std::vector<std::string>& options) {
-        if (options.empty()) return "";
-        // Divide 360 degrees
-        float segment = 360.0f / options.size();
-        int index = std::floor(degrees / segment);
-
-        //Clamp Index to prevent out of bounds at 360
-        if (index >= options.size()) index = options.size() - 1;
-        return options[index];
-    }
-    //translate raw inputs into readable language
-    std::string translate(std::string input){
-        if (input.empty()) return "";
-
-        std::string result = "";
-        // Process chunks likes c024
-        for (size_t i = 0; i < input.length(); i+= 4) {
-            char identity = input [i];
-            int degrees = std::stoi(input.substr(i + 1, 3);
-
-            switch (identity) {
-                case 'n': //Numbers
-                    result += pickFromCircle(degrees, nums);
-                    break;
-                case 'e': //Equations
-                    result += pickFromCircle(degrees, eq);
-                    break;
-                case 'c': //Conditionals
-                    result += pickFromCircle(degrees, cond);
-                    break;
-                case 's': // Special characters
-                    result += pickFromCircle(degrees, special);
-                    break;
-                case 'a': //Variables (abc)
-                    result += "a"; //Placehold for alphabet logic
-                    break;
-            }
+        //used to trim extra spaces
+        std::string trim(const std::string& str) {
+            auto start = str.begin();
+            while (start != str.end() && std::isspace(*start)) ++start;
+            auto end = str.end();
+            do { --end; } while (end != start && std::isspace(*end));
+            return std::string(start, end + 1);
         }
-        return result;
-    }
-    void run(std::string input){
-        std::string code = translate(input);
-        std::cout<< "Translating Code: " << code << std::endl;
-    }
+        template <typename T>
+        std::string toString(const T& value) {
+            std::ostringstream oss;
+            oss << value;
+            return oss.str();
+        }
+
+        //translate raw inputs into readable language
+        std::string translate(std::string input){
+            return toString(cat.getCat(input[0],input.substr(1)));
+        }
+        void run(std::string input){
+            //TODO: translate, if there multiple inputs in a line, convert each one
+            //TODO: check for skip lines
+            //recommend do loop till end of string
+            std::string temp = "";
+            while(input != "") {
+                if (input.front() == ' '){
+                    //TODO: handle spaces
+                    input.erase(0,1);
+                }
+                temp.append(translate(input.substr(0,3)));
+                input.erase(0,3);
+            }
+            input = temp; // input now holds translated version of line
+            //TODO: do libary here
+            //RECOMMENDED: use of env
+        }
 
     public:
         void runLive(){
@@ -84,7 +64,7 @@ class CBC {
                 run(trim(line));
             }
 
-        file.close();
-        return;
-    }
+            file.close();
+            return;
+        }
 };
