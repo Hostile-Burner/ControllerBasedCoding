1dba30df772c4436c9b62382197b233c9ba22c58
Hostile_Burner <161495934+Hostile-Burner@users.noreply.github.com>
2026-05-04 20:54:48 -0700

    after talk with project parter
    Finished majority of Categories, a bit scuff.
    changed a bunch of code to make sure  everything currently worked correctly, subject to change.
    
    TODO:
    CBCLanguage.cpp
    controller inputs
    Libary

--- BackEnd/Language/CBCLanguage.cpp
+++ BackEnd/Language/CBCLanguage.cpp
@@ -2,11 +2,12 @@
 #include <unordered_map>
 #include <fstream>
 #include <string>
-#include "Catagories.cpp"
+#include "Categories.cpp"
 
 class CBC {
     private:
         std::unordered_map<int, std::string> env;
+        Category cat;
 
         //used to trim extra spaces
         std::string trim(const std::string& str) {
@@ -16,21 +17,38 @@ class CBC {
             do { --end; } while (end != start && std::isspace(*end));
             return std::string(start, end + 1);
         }
+        template <typename T>
+        std::string toString(const T& value) {
+            std::ostringstream oss;
+            oss << value;
+            return oss.str();
+        }
 
         //translate raw inputs into readable language
         std::string translate(std::string input){
-            
-
+            return toString(cat.getCat(input[0],input.substr(1)));
         }
         void run(std::string input){
-            input = translate(input);
-
-
+            //TODO: translate, if there multiple inputs in a line, convert each one
+            //TODO: check for skip lines
+            //recommend do loop till end of string
+            std::string temp = "";
+            while(input != "") {
+                if (input.front() == ' '){
+                    //TODO: handle spaces
+                    input.erase(0,1);
+                }
+                temp.append(translate(input.substr(0,3)));
+                input.erase(0,3);
+            }
+            input = temp; // input now holds translated version of line
+            //TODO: do libary here
+            //RECOMMENDED: use of env
         }
 
     public:
         void runLive(){
-
+            std::cout << "This usage has not been implemented.";
         }
         void runFile(std::string fileName){
             std::ifstream file(fileName);
